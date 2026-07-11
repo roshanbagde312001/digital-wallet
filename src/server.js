3405beae5c8a6ad765ba3089d2501276b894e006
roshanbagde312001 <jitcse.roshanbagde@gmail.com>
2026-07-11 19:12:07 +0530

    feat: configure mysql database connection

--- src/server.js
+++ src/server.js
@@ -1,7 +1,19 @@
-const app = require("./app")
+require("dotenv").config();
+const app = require("./app");
+const sequelize = require("./config/database");
 
 const PORT = process.env.PORT || 9000;
 
-app.listen(PORT, () => {
+app.listen(PORT, async() => {
     console.log(`Server running on port ${PORT}`);
+    try{
+        await sequelize.authenticate();
+        console.log("Database conected")
+    }
+     catch(error){
+
+        console.log("Database connection failed");
+        console.log(error.message);
+
+    }
 });
